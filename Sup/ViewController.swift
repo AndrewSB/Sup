f54fbe202f96f8208b6d73497d32887cbab451ca
AndrewSB <asbreckenridge@me.com>
2014-12-28 20:07:57 -0800

    add railway gif background

--- Sup/ViewController.swift
+++ Sup/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        addBackground()
+        
         let authenticateButton = DGTAuthenticateButton(authenticationCompletion: {
             (session: DGTSession!, error: NSError!) in
             // play with Digits session
@@ -20,11 +23,16 @@ class ViewController: UIViewController {
         authenticateButton.center = self.view.center
         self.view.addSubview(authenticateButton)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func addBackground() {
+        let filePath = NSBundle.mainBundle().pathForResource("railway", ofType: "gif")
+        
+        let webBG = UIWebView(frame: self.view.frame)
+        webBG.loadData(NSData(contentsOfFile: filePath!), MIMEType: "image/gif", textEncodingName: nil, baseURL: nil)
+        webBG.userInteractionEnabled = false
+        self.view.addSubview(webBG)
     }
+
 
 
 }
