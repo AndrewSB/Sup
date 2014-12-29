63247994b8da88d261b34f9abd6bbb0042a2b819
AndrewSB <asbreckenridge@me.com>
2014-12-28 19:16:04 -0800

    init

---
+++ Sup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sup
+//
+//  Created by Andrew Breckenridge on 12/28/14.
+//  Copyright (c) 2014 asb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
