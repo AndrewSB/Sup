13457b78c1b8c1f3bc4245a7b2514d9846ff5617
AndrewSB <asbreckenridge@me.com>
2014-12-28 23:51:29 -0800

    add colored buttons

--- Sup/LoginViewController.swift
+++ Sup/LoginViewController.swift
@@ -7,14 +7,20 @@
 //
 
 import UIKit
+import QuartzCore
 
 class LoginViewController: UIViewController {
     @IBOutlet weak var webBG: UIWebView!
     @IBOutlet weak var logoNameLabel: UILabel!
+    @IBOutlet weak var phoneButton: UIButton!
+    @IBOutlet weak var emailButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         addBackground()
+        
+        // Using pallete http://www.colourlovers.com/palette/2316406/G2K_Followers
+
         logoNameLabel.transform = (CGAffineTransformMakeRotation(2*CGFloat(M_PI)))
     }
     
