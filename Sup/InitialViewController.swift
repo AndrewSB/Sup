176c4f9e9ec7ee60cdbfcf3be8571f2b71dc97dd
AndrewSB <asbreckenridge@me.com>
2014-12-29 19:12:41 -0800

    connect IB to VC files + create launchScreen xib

---
+++ Sup/InitialViewController.swift
@@ -0,0 +1,13 @@
+//
+//  InitialViewController.swift
+//  Sup
+//
+//  Created by Andrew Breckenridge on 12/29/14.
+//  Copyright (c) 2014 asb. All rights reserved.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+}
---
+++ Sup/SignupViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SignupViewController.swift
+//  Sup
+//
+//  Created by Andrew Breckenridge on 12/29/14.
+//  Copyright (c) 2014 asb. All rights reserved.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+}
