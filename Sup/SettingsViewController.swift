264e3b750f5dfb550afdd219d38990c7ac5b668f
AndrewSB <asbreckenridge@me.com>
2014-12-30 15:28:28 -0800

    add the paged based vcs for the main view and connect to storyboard

---
+++ Sup/SettingsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.swift
+//  Sup
+//
+//  Created by Andrew Breckenridge on 12/30/14.
+//  Copyright (c) 2014 asb. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+}
