89c407fc608dd60395a063f691049d2d68c22c3d
AndrewSB <asbreckenridge@me.com>
2014-12-30 00:36:08 -0800

    add sign up and login text fields

--- Sup/SignupViewController.swift
+++ Sup/SignupViewController.swift
@@ -9,5 +9,41 @@
 import UIKit
 
 class SignupViewController: UIViewController {
-
+    override func loadView() {
+        CATransaction.begin()
+        CATransaction.setDisableActions(true)
+        
+        navigationController?.navigationBar.barTintColor = UIColor(red: 1, green: 0.293, blue: 0.279, alpha: 1)
+        self.navigationController?.navigationBar.translucent
+        println(self.navigationController?.navigationBar.translucent)
+        CATransaction.commit()
+        
+        super.loadView()
+    }
+    
+    override func viewDidLoad() {
+        self.navigationController?.setNavigationBarHidden(false, animated: false)
+        
+        let backButton = UIBarButtonItem(title: "<", style: .Plain, target: self, action: "unwind")
+        
+        self.navigationItem.setHidesBackButton(true, animated: false)
+        self.navigationItem.setLeftBarButtonItem(backButton, animated: true)
+    }
+    
+    override func segueForUnwindingToViewController(toViewController: UIViewController, fromViewController: UIViewController, identifier: String?) -> UIStoryboardSegue {
+        println("custom unwinded")
+        let segue = PushNoAnimationSegue(identifier: identifier, source: fromViewController, destination: toViewController)
+        return segue
+    }
+    
+    func unwind() {
+        println("unwinded")
+        
+        CATransaction.begin()
+        CATransaction.setDisableActions(true)
+        navigationController?.navigationBar.barTintColor = UIColor(red: 0.136, green: 0.859, blue: 0.112, alpha: 1)
+        CATransaction.commit()
+        
+        performSegueWithIdentifier("unwindToWelcome", sender: self)
+    }
 }
