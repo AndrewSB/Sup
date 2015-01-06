d1cdbd2a7f85ce43e675dbfdaed96e00ab418f84
Andrew Breckenridge <asbreckenridge@me.com>
2015-01-05 20:09:30 -0800

    change and pbfuscate keys

--- Sup/AppDelegate.swift
+++ Sup/AppDelegate.swift
@@ -19,7 +19,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         Fabric.with([Crashlytics()])
         
-        Parse.setApplicationId("fCots4c5CyFuKxLchx8JvYrC9NgmUoWqZVmbzBPj", clientKey: "qcMWYJoWmgqcLaks8Tq6I5btKTB25elTH1bzh3Ik")
+        if let keys = NSBundle.mainBundle().pathForResource("API-Keys", ofType: "plist") {
+            let rootDict = NSDictionary(contentsOfFile: keys)
+            let p = rootDict!["Parse"] as Dictionary<String, String>
+            Parse.setApplicationId(p["ApplicationID"], clientKey: p["ClientKey"])
+        } else {
+            fatalError("You don't have access to the API Keys")
+        }
+
         PFAnalytics.trackAppOpenedWithLaunchOptionsInBackground(launchOptions, block: nil)
         
         let attributes = [NSFontAttributeName as NSObject: UIFont(name: "Avenir-Black", size: 15) as AnyObject!, NSForegroundColorAttributeName: UIColor.whiteColor()]
